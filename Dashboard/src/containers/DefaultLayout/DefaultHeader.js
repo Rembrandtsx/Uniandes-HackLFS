d0e39ad70b6f54760f6d0d88048fd454d84a594c
Juliana Prieto <jprietoarcila@gmail.com>
2019-10-26 12:34:54 -0500

    Dashboard

---
+++ Dashboard/src/containers/DefaultLayout/DefaultHeader.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Badge, UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+import { AppAsideToggler, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+import logo from '../../assets/img/brand/Adventio.png'
+import sygnet from '../../assets/img/brand/sygnet.svg'
+
+const propTypes = {
+  children: PropTypes.node,
+};
+
+const defaultProps = {};
+
+class DefaultHeader extends Component {
+  render() {
+
+    // eslint-disable-next-line
+    const { children, ...attributes } = this.props;
+
+    let imgStyle = {
+      width: '100px',
+      marginLeft: '20px'
+    }
+
+    return (
+      <React.Fragment>
+        <AppSidebarToggler className="d-lg-none" display="md" mobile />
+        {/* <AppNavbarBrand
+          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
+          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+        /> */}
+        <img src={logo} style={imgStyle}/>
+        <AppSidebarToggler className="d-md-down-none" display="lg" />
+
+        <Nav className="d-md-down-none" navbar>
+          <NavItem className="px-3">
+            <NavLink to="/dashboard" className="nav-link" >Dashboard</NavLink>
+          </NavItem>
+          <NavItem className="px-3">
+            <Link to="/users" className="nav-link">Usuarios</Link>
+          </NavItem>
+        </Nav>
+        <Nav className="ml-auto" navbar>
+          <NavItem className="d-md-down-none">
+            <NavLink to="#" className="nav-link"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav direction="down">
+            <DropdownToggle nav>
+              <img src={'../../assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem header tag="div" className="text-center"><strong>Cuenta</strong></DropdownItem>
+              <DropdownItem><i className="fa fa-user"></i> Perfil</DropdownItem>
+              <DropdownItem><i className="fa fa-usd"></i> Pagos<Badge color="secondary">42</Badge></DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Cerrar sesión</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </React.Fragment>
+    );
+  }
+}
+
+DefaultHeader.propTypes = propTypes;
+DefaultHeader.defaultProps = defaultProps;
+
+export default DefaultHeader;
