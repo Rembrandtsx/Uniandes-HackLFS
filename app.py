fec99426072eda4eb934da7e03f31120b0ff9b5f
lala341 <li.forero@uniandes.edu.co>
2019-10-26 04:37:00 -0500

    Cambios back

---
+++ app.py
@@ -0,0 +1,62 @@
+# app.py
+from flask import Flask, request, jsonify
+from firebase import firebase
+import os
+firebase = firebase.FirebaseApplication('https://hackandes-1816a.firebaseio.com/', None)
+app = Flask(__name__)
+
+@app.route('/data1/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+
+    response = {}
+
+
+    result = firebase.get('/hackandes-1816a/Test/', '')
+    print(result)
+
+
+    response=result
+
+
+    """"# Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+"""
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/data/', methods=['POST'])
+def post_something():
+    """param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    """
+
+
+    data =  { 'Name': 'Vivek',
+          'RollNo': 1,
+          'Percentage': 76.02
+          }
+    result = firebase.post('/hackandes-1816a/Test/',data)
+    print(result)
+    return result
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    port = int(os.environ.get("PORT", 5000))
+    app.run(threaded=True, port=port)
